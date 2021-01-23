38e53ec089cc25dadd464e55dc3ffadee43eafda
Katsuhiro <sc30.katsu@gmail.com>
2021-01-23 17:49:16 +0900

    .feat Flask templates

--- Flask/app.py
+++ Flask/app.py
@@ -1,10 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-  name = 'Hello World!'
-  return name
+def index():
+  return render_template('index.html')
+
+@app.route('/home/<string:user_name>/<int:age>')
+def home(user_name, age):
+  # login_user = user_name
+  login_user = {
+    'name' : user_name,
+    'age'  : age,
+  }
+  return render_template('home.html', login_user = login_user)
+
+@app.route('/userlist')
+def user_list():
+  user_list = [
+    'Taro', 'Jiro', 'Saburo', 'Shiro'
+  ]
+  return render_template('userlist.html', users = user_list)
 
 @app.route('/good')
 def good():
