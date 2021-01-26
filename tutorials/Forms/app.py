eda140acc580ab3a85f0769a2394818fa0ab2fcd
Katsuhiro <sc30.katsu@gmail.com>
2021-01-26 22:13:24 +0900

    .feat image upload

--- tutorials/Forms/app.py
+++ tutorials/Forms/app.py
@@ -1,5 +1,8 @@
 from types import MethodDescriptorType
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
+from werkzeug.utils import secure_filename
+import pykakasi
+import os
 
 app = Flask(__name__)
 
@@ -37,6 +40,20 @@ def home():
   )
 
   return render_template('home.html', user_info = user_info)
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+  if request.method == 'GET':
+    return render_template('upload.html')
+  elif request.methodd == 'POST':
+    file = request.files['file']
+    save_filename = secure_filename(file.filename)
+    file.save(os.path.join('./static/image', save_filename))
+    return redirect(url_for('upload_file', filename = save_filename))
+
+@app.route('/upload_file<string:filename>')
+def uploaded_file(filename):
+  return render_template('uploaded_file.html', filename = filename)
 
 if __name__ == '__main__':
   app.run(debug=True)
