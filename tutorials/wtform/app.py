6648833cceb32e5d0f2246e5b6273f2d871fdf43
Katsuhiro <sc30.katsu@gmail.com>
2021-01-31 20:30:52 +0900

    .feat install and tutorial of wtforms

---
+++ tutorials/wtform/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request
+from wtforms import StringField, SubmitField, IntegerField
+from wtforms.form import Form
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = ''
+
+class UserForm(Form):
+  name = StringField('名前')
+  age  = IntegerField('年齢')
+  submit = SubmitField('Submit')
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+  name = age = ''
+  form = UserForm(request.form)
+  if request.method == 'POST':
+    if form.validate():
+      name = form.name.data
+      age = form.age.data
+      form.name.data = ''
+      form.age.data  = ''
+    else:
+      print('入力内容に問題があります')
+
+  return render_template('index.html', form=form, name=name, age=age)
+
+if __name__ == '__main__':
+  app.run(debug=True)
